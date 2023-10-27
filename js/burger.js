0312a9f157c8c4d682f60ee93813e8fc1292edde
Mykhailo Yaremchuk <mikhailya4@gmail.com>
2023-10-27 23:27:51 +0300

    header but with wisible site under the header

--- js/burger.js
+++ js/burger.js
@@ -13,4 +13,18 @@ function pushBurgerButton() {
     }
 }  
 burgerButton.addEventListener("click", pushBurgerButton);
-
+// Звертання бургеру при натисканні не на лінк
+const navLink = document.querySelectorAll('.nav__list-link');
+function removeBurgerClasses() {
+    if(burgerWrapper.classList.contains("active")) {
+        burgerWrapper.classList.remove("active");
+        body.classList.remove("lock");
+        burgerButton.classList.remove("active");
+    }
+}
+navLink.forEach(function(item) {
+  item.addEventListener('click', removeBurgerClasses);
+});
+// logo прибирає класи
+const navLogo = document.querySelector('.header__logo');
+navLogo.addEventListener('click', removeBurgerClasses);
